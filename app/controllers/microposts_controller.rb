9735ad484a2037f48c989803eb15343bbd84a9a3
dwap392 <mi3ku9ni27@gmail.com>
2016-02-21 06:03:38 +0000

    add posting microposts2

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-    before_action :logged_in_user, only: [:create]
+    before_action :logged_in_user, only: [:create, :destroy]
     
     def create
         @micropost = current_user.microposts.build(micropost_params)
@@ -13,7 +13,8 @@ class MicropostsController < ApplicationController
     
     def destroy
       @micropost = current_user.microposts.find_by(id: params[:id])
-      return redirect_to root_url if @mi.nil?
+      return redirect_to root_url if @micropost.nil?
+
       @micropost.destroy
       flash[:success] = "Micropost deleted"
       redirect_to request.referrer || root_url
